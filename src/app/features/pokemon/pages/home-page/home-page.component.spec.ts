987ee4f2ff53e3dad0bf0a5d05e095911a64e330
Nicolas Erazo <nico.lacho@outlook.com>
2025-08-09 19:50:21 -0500

    Update Final EsLint

--- src/app/features/pokemon/pages/home-page/home-page.component.spec.ts
+++ src/app/features/pokemon/pages/home-page/home-page.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PokeTableComponent } from './home-page.component';
+import { HomePageComponent } from './home-page.component';
 
-describe('PokeTableComponent', () => {
-  let component: PokeTableComponent;
-  let fixture: ComponentFixture<PokeTableComponent>;
+describe('HomePageComponent', () => {
+  let component: HomePageComponent;
+  let fixture: ComponentFixture<HomePageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PokeTableComponent],
+      declarations: [HomePageComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(PokeTableComponent);
+    fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
